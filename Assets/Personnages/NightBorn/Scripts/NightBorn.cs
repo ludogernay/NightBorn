4beab0ab29802ead814831f05c21ff590dd9dccc
ludogernay <ludovic.gernay@ynov.com>
2023-11-22 16:10:22 +0100

    [+] Attack animation

--- Assets/Personnages/NightBorn/Scripts/NightBorn.cs
+++ Assets/Personnages/NightBorn/Scripts/NightBorn.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class NightBorn : MonoBehaviour
 {
-    public Rigidbody2D nightBornRB;
+    private Rigidbody2D nightBornRB;
     public Transform groundCheckLeft;
     public Transform groundCheckRight;
     public float jumpForce;
     private int maxJumps = 1; // Nombre maximum de sauts
     private bool isGrounded;
     private int jumpsLeft;
+    public Animator AttackAnim;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        nightBornRB=GetComponent<Rigidbody2D>();
         jumpsLeft = maxJumps;
     }
 
@@ -30,6 +32,11 @@ public class NightBorn : MonoBehaviour
         {
             maxJumps = 2;
         }
+        if (Input.GetMouseButtonDown(0)){
+            AttackAnim.SetBool("attack",true);
+        }else{
+            AttackAnim.SetBool("attack",false);
+        }
         if (isGrounded && (nightBornRB.velocity.y == 0f))
         {
             jumpsLeft = maxJumps;
@@ -38,7 +45,6 @@ public class NightBorn : MonoBehaviour
 
     void PlayerJump()
     {
-        Debug.Log(isGrounded);
         nightBornRB.velocity = new Vector2(nightBornRB.velocity.x, 0f); // Réinitialise la vélocité en y avant de sauter
         nightBornRB.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         jumpsLeft--; // Décrémente jumpsLeft si le joueur est en l'air
