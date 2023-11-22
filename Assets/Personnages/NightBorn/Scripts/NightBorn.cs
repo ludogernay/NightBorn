27112cc4e2265a03b8ad47b18de930d03034dc6e
LamaShin <c.blairon-lacoste@hotmail.fr>
2023-11-22 16:05:47 +0100

    [+] AJout PlayerAttack() et Collider Epee

--- Assets/Personnages/NightBorn/Scripts/NightBorn.cs
+++ Assets/Personnages/NightBorn/Scripts/NightBorn.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class NightBorn : MonoBehaviour
 {
     public Rigidbody2D nightBornRB;
+
+    public BoxCollider2D NightBorn_Sword;
+
+    public float NightBorn_Sword_AttackDuration = 1.0f;
+
+    public float NightBorn_Sword_AttackCooldown = 1.0f;
+
+    public bool NightBorn_SwordIsActive = false;
     public Transform groundCheckLeft;
     public Transform groundCheckRight;
     public float jumpForce;
     private int maxJumps = 1; // Nombre maximum de sauts
     private bool isGrounded;
     private int jumpsLeft;
-
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +28,17 @@ public class NightBorn : MonoBehaviour
 
     void Update()
     {
+
+        if (Input.GetMouseButtonDown(0) && NightBorn_Sword_AttackCooldown <= 0)
+        {
+            PlayerAttack();
+        }
+        else
+        {
+            EndPlayerAttack();
+        }
+
+
         isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
         if (Input.GetButtonDown("Jump") && jumpsLeft > 0)
         {
@@ -38,10 +56,34 @@ public class NightBorn : MonoBehaviour
 
     void PlayerJump()
     {
-        Debug.Log(isGrounded);
+        Debug.Log("Night_Born is on Ground : " + isGrounded);
         nightBornRB.velocity = new Vector2(nightBornRB.velocity.x, 0f); // Réinitialise la vélocité en y avant de sauter
         nightBornRB.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         jumpsLeft--; // Décrémente jumpsLeft si le joueur est en l'air
     }
 
+    void PlayerAttack()
+    {
+        if (!NightBorn_SwordIsActive)
+        {
+            NightBorn_Sword_AttackCooldown = 1.0f;
+            NightBorn_Sword_AttackDuration = 1.0f;
+            Debug.Log("Night_Born Attack");
+            NightBorn_Sword.enabled = true;
+            NightBorn_SwordIsActive = true;
+            Debug.Log("NightBorn_SwordColliderEnabled : " + NightBorn_Sword.enabled);
+        }
+    }
+
+    void EndPlayerAttack()
+    {
+        NightBorn_Sword_AttackCooldown -= Time.deltaTime;
+        NightBorn_Sword_AttackDuration -= Time.deltaTime;
+        if (NightBorn_Sword_AttackDuration <= 0)
+        {
+            NightBorn_Sword.enabled = false;
+            NightBorn_SwordIsActive = false;
+            Debug.Log("NightBorn_SwordColliderEnabled : " + NightBorn_Sword.enabled);
+        }
+    }
 }
