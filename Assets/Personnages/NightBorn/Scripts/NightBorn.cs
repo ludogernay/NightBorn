d02835b4ecc25ac45f5ba43c8d8df92aebfd0b49
ludogernay <ludovic.gernay@ynov.com>
2023-11-22 10:40:47 +0100

    Prepare for merge into main

--- Assets/Personnages/NightBorn/Scripts/NightBorn.cs
+++ Assets/Personnages/NightBorn/Scripts/NightBorn.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class NightBorn : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float jumpForce;
+    private bool isJumping = false;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+
+        if(Input.GetButtonDown("Jump")){
+            isJumping = true;
+        }
+        PlayerJump();
+    }
+    void PlayerJump(){
+        if(isJumping){
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = false;
+        }
     }
 }
