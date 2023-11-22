1353e7f289648a1c4f0d0c3dc4b721f19513c771
ludogernay <ludovic.gernay@ynov.com>
2023-11-22 14:39:13 +0100

    [+] Double jump

--- Assets/Personnages/NightBorn/Scripts/NightBorn.cs
+++ Assets/Personnages/NightBorn/Scripts/NightBorn.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class NightBorn : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    public Rigidbody2D nightBornRB;
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
     public float jumpForce;
-    private bool isJumping = false;
+    private int maxJumps = 1; // Nombre maximum de sauts
+    private bool isGrounded;
+    private int jumpsLeft;
+
+
     // Start is called before the first frame update
-
-    void Update(){
-
-        if(Input.GetButtonDown("Jump")){
-            isJumping = true;
-        }
-        PlayerJump();
+    void Start()
+    {
+        jumpsLeft = maxJumps;
     }
-    void PlayerJump(){
-        if(isJumping){
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            isJumping = false;
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        if (Input.GetButtonDown("Jump") && jumpsLeft > 0)
+        {
+            PlayerJump();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            maxJumps = 2;
+        }
+        if (isGrounded && (nightBornRB.velocity.y == 0f))
+        {
+            jumpsLeft = maxJumps;
         }
     }
+
+    void PlayerJump()
+    {
+        Debug.Log(isGrounded);
+        nightBornRB.velocity = new Vector2(nightBornRB.velocity.x, 0f); // Réinitialise la vélocité en y avant de sauter
+        nightBornRB.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        jumpsLeft--; // Décrémente jumpsLeft si le joueur est en l'air
+    }
+
 }
