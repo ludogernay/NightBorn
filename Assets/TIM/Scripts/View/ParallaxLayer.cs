bd7488741ba90ed10eb7a03e660dd51805e5100a
Fr33Ze06 <118484751+Fr33Ze06@users.noreply.github.com>
2024-01-10 11:15:30 +0100

    [~] Update background

--- Assets/TIM/Scripts/View/ParallaxLayer.cs
+++ Assets/TIM/Scripts/View/ParallaxLayer.cs
@@ -6,23 +6,43 @@ namespace Platformer.View
     /// Used to move a transform relative to the main camera position with a scale factor applied.
     /// This is used to implement parallax scrolling effects on different branches of gameobjects.
     /// </summary>
+    /// 
     public class ParallaxLayer : MonoBehaviour
     {
         /// <summary>
         /// Movement of the layer is scaled by this value.
         /// </summary>
+        // public Vector3 movementScale = Vector3.one;
+
+        // public float scrollspeed = 0.25f;
+        // Transform _camera;
+
+        // void Awake()
+        // {
+        //     _camera = Camera.main.transform;
+        // }
+
+        // void LateUpdate()
+        // {
+        //     Debug.Log(_camera.position);
+        //     transform.position = Vector3.Scale(_camera.position, movementScale);
+        // }
+
         public Vector3 movementScale = Vector3.one;
-
-        Transform _camera;
-
-        void Awake()
-        {
-            _camera = Camera.main.transform;
-        }
+        public float scrollSpeed = 0.25f;
+        public Transform ground; // Référence au sol ou à un autre objet que vous souhaitez suivre
 
         void LateUpdate()
         {
-            transform.position = Vector3.Scale(_camera.position, movementScale);
+            if (ground == null)
+            {
+                Debug.LogError("Ground reference is not set in ParallaxLayer script!");
+                return;
+            }
+
+            // Obtenez la position du sol et appliquez le facteur d'échelle et la vitesse de défilement
+            Vector3 targetPosition = new Vector3(ground.position.x, ground.position.y+5, transform.position.z);
+            transform.position = Vector3.Scale(targetPosition, movementScale) * scrollSpeed;
         }
 
     }
--- Assets/Terrain/Scripts/Sol/MoveFloor.cs
+++ Assets/Terrain/Scripts/Sol/MoveFloor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveFloor : MonoBehaviour
 {
     public GameObject wall;
+    public GameObject Background;
 
     public float speedGround ;
     // Start is called before the first frame update
