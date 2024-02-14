f33556b70da99147d3be42d33acda9276c3a82c3
ludogernay <117349092+ludogernay@users.noreply.github.com>
2024-02-14 11:31:08 +0100

    [+] menu pause

--- Assets/Jeu/Scripts/Pause.cs
+++ Assets/Jeu/Scripts/Pause.cs
@@ -1,36 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
     public GameObject pauseMenu;
-    public bool isPaused;
-    void Start()
-    {
-        pauseMenu.SetActive(false);
-    }
+    private bool isPaused = false;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isPaused)
-            {
                 Resume();
-            }
             else
-            {
                 PauseGame();
-            }
         }
     }
+
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
     }
+
     public void PauseGame()
     {
         pauseMenu.SetActive(true);
