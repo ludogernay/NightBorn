7b70ac7b099ce10afbde95b043fde0d32e86030c
LoicHarroue <116339730+LoicHarroue@users.noreply.github.com>
2024-02-28 14:58:22 +0100

    ennemy movment

--- Assets/Personnages/Ennemis/Scripts/EnnemyMovement.cs
+++ Assets/Personnages/Ennemis/Scripts/EnnemyMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class EnnemyMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+    public float speed;
+    [SerializeField] private string drone;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = transform.Find(drone).GetComponent<Rigidbody2D>();
+        anim = transform.Find(drone).GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(-speed, 0);
     }
 }
